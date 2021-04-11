8ea8df80facc613106b53918fe84ef9f47928fc5
Esteban Castano <esteban.crw@gmail.com>
2021-04-11 10:13:59 -0500

    feat(web): Vue app for viewing pools

---
+++ web/src/App.vue
@@ -0,0 +1,31 @@
+<template>
+  <pools-table :pools="pools"></pools-table>
+</template>
+
+<script>
+import PoolsTable from '@/components/PoolsTable.vue'
+import pools from '../mocks/pools'
+
+export default {
+  name: 'App',
+  components: {
+    PoolsTable,
+  },
+  data() {
+    return {
+      pools,
+    }
+  },
+}
+</script>
+
+<style>
+#app {
+  font-family: Avenir, Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+  margin-top: 60px;
+}
+</style>
---
+++ web/src/components/PoolsTable.vue
@@ -0,0 +1,45 @@
+<template>
+  <table>
+    <thead>
+      <tr>
+        <th>Project</th>
+        <th>Name</th>
+        <th>aprYear</th>
+      </tr>
+    </thead>
+    <tbody>
+      <tr
+        v-for="pool in sortedFilteredPools"
+        :key="pool.name"
+      >
+        <td>{{ pool.project }}</td>
+        <td>{{ pool.name }}</td>
+        <td>{{ pool.aprYear }}</td>
+      </tr>
+    </tbody>
+  </table>
+</template>
+
+<script>
+export default {
+  props: {
+    pools: {
+      type: Array,
+      required: true,
+    },
+  },
+  computed: {
+    filteredPools() {
+      return this.pools.filter(
+        (pool) => pool.aprYear > 300
+      )
+    },
+    sortedFilteredPools() {
+      const copyPools = this.filteredPools
+      return copyPools.sort(
+        (a, b) => b.aprYear - a.aprYear
+      )
+    },
+  },
+}
+</script>
